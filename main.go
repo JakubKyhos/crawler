50c02334f55535867ac3db61b5ebbfac9321c91e
JakubKyhos <kubakyhos4@gmail.com>
2025-06-06 12:41:38 +0200

    main update

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	} else {
+		fmt.Printf("starting crawl of: %s ", os.Args[1])
+	}
+
 }
